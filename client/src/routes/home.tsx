39b833614fca4a273c0f89adb9eab3e4eb958322
RedCrafter07 <github@redcrafter07.de>
2022-07-22 20:44:06 +0200

    Main page

---
+++ client/src/routes/home.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+	return (
+		<div className='bg-zinc-800 text-white min-h-screen p-2'>
+			<div className='mx-auto container'>
+				<p>Hi.</p>
+			</div>
+		</div>
+	);
+}
