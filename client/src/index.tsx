30b9884e48e90f4a343e569cc03171eb600b0cf7
RedCrafter07 <github@redcrafter07.de>
2022-08-11 13:22:01 +0200

    Add Wrapper Element

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,8 +1,8 @@
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Home from './routes/home';
+import Wrappers from './wrappers';
 
 const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement,
 );
-root.render(<Home />);
+root.render(<Wrappers />);
